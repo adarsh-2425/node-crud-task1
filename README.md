a698696ae9312e3d54fb11d84206b7cb6040ede0
adarsh-2425 <adarsh.lol2425@gmail.com>
2023-09-29 22:26:26 +0530

    readme

--- README.md
+++ README.md
@@ -1,7 +1,40 @@
 # Node CRUD Task 1
 
-## Requirements
-- Sign Up - Email Password Profile Image,
-- Log In - Email Password - with JWT ,
-- Profile ,
+## Requirements of Task
+- Sign Up - Email, Password, Profile Image.
+- Log In - Email, Password - with JWT.
+- Profile
 - Update Profile
+
+### Installation
+
+1. Clone the repository:
+
+   ```shell
+   git clone https://github.com/adarsh-2425/node-crud-task1.git
+   cd node-crud-task1
+   ```
+2. Install dependencies:
+    ```shell
+    npm i
+    ```
+
+3. Configure your MongoDB connection in src/config/keys.ts.
+
+4. Start the server
+    ```shell
+    npm start
+    ```
+
+## API Endpoints
+
+### Authentication Routes
+
+- `POST /api/auth/signup`: Register a new user.
+- `POST /api/auth/login`: Authenticate user and generate JWT token.
+
+### Profile Routes
+
+- `GET /api/profile/dashboard`: Profile
+- `PUT /api/profile/update`: Update Profile
+
--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const app = express();
 
-//const multer = require('multer');
-//const upload = multer(); 
-
 // env file access
 require('dotenv').config()
 
@@ -14,30 +11,16 @@ const PORT = 3000 || process.env.PORT;
 const connectDB = require("./services/db-connection");
 connectDB();
 
-//Middleware for parsing
+//Middleware for parsing json data
 app.use(express.json());
-//app.use(upload.fields([])); // Parse only fields
 
-// SignUp Route
-const signUpRoute = require("./routers/auth-route");
-app.use('/auth', signUpRoute);
-
-// Login Route
-const loginRoute = require("./routers/auth-route");
-app.use('/auth', loginRoute);
+//auth route
+const authRoute = require("./routers/auth-route");
+app.use('/api/auth', authRoute);
 
 // Profile Route
-const dashboard = require("./routers/profile-route");
-app.use('/profile', dashboard);
-
-// Update Profile Route
-const updateProfileRoute = require("./routers/profile-route");
-app.use('/profile', updateProfileRoute)
-
-// home route
-app.get('/', (req, res) => {
-    res.send({message: "Hi fromn Node App"})
-})
+const profileRoute = require("./routers/profile-route");
+app.use('/api/profile', profileRoute);
 
 //error handling middleware
 app.use((err, req, res, next) => {
@@ -45,6 +28,7 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something went wrong');
   });
 
+// Server
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
